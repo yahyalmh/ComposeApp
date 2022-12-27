55c57348b541e9bde186e6f8e71483ad9c8b1fb3
Yahya Hassani <yahyamh01@gmail.com>
2022-12-27 14:00:54 +0100

    Improve retry views

--- ui/common/src/main/java/com/example/common/component/AutoRetryView.kt
+++ ui/common/src/main/java/com/example/common/component/AutoRetryView.kt
@@ -5,13 +5,13 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -20,22 +20,17 @@ import androidx.compose.ui.unit.dp
 import com.example.common.component.icon.AppIcons
 import com.example.ui.common.R
 
-
-/**
- * @author yaya (@yahyalmh)
- * @since 10th November 2022
- */
-
 @Composable
-fun RetryView(
+fun AutoRetryView(
     modifier: Modifier = Modifier,
     isVisible: Boolean = true,
     errorMessage: String,
-    icon: ImageVector, onRetry: () -> Unit
+    icon: ImageVector,
+    hint: String = stringResource(id = R.string.autoRetryHint),
 ) {
-    AnimatedVisibility(visible = isVisible,
-        enter = fadeIn(),
-        exit = fadeOut()) {
+    AnimatedVisibility(
+        visible = isVisible, enter = fadeIn(), exit = fadeOut()
+    ) {
         Column(
             modifier = modifier
                 .background(MaterialTheme.colorScheme.background)
@@ -49,16 +44,15 @@ fun RetryView(
                     .padding(12.dp)
                     .size(80.dp),
                 imageVector = icon,
-                contentDescription = "retry icon",
+                contentDescription = stringResource(id = R.string.autoRetryIconDescription),
                 tint = MaterialTheme.colorScheme.error
             )
 
-            Spacer(modifier = modifier.height(10.dp))
-
-
             Text(
                 text = errorMessage,
-                modifier = modifier.fillMaxWidth(),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(12.dp),
                 textAlign = TextAlign.Center,
                 style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.colorScheme.error
@@ -66,15 +60,24 @@ fun RetryView(
 
             Spacer(modifier = modifier.height(10.dp))
 
-            Button(onClick = onRetry) {
-                Text(text = stringResource(R.string.retry))
-            }
+            Text(
+                text = hint,
+                modifier = modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Green
+            )
+
         }
     }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun RetryPreview() {
-    RetryView(icon = AppIcons.Warning, errorMessage = "This is a error message") {}
+fun AutoRetryPreview() {
+    AutoRetryView(
+        icon = AppIcons.Warning,
+        errorMessage = "This is a error message",
+        hint = "Go online to try again"
+    )
 }

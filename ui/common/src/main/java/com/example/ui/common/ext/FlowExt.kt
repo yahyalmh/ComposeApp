fc9e65ac58aa57c2b89533a2ee1cd05b1736a536
Yahya Hassani <yahyamh01@gmail.com>
2023-01-02 16:59:17 +0100

    Add retry when internet becomes available extension function

--- ui/common/src/main/java/com/example/ui/common/ext/FlowExt.kt
+++ ui/common/src/main/java/com/example/ui/common/ext/FlowExt.kt
@@ -1,10 +1,11 @@
 package com.example.ui.common.ext
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.retryWhen
+import com.example.ui.common.connectivity.ConnectivityMonitor
+import kotlinx.coroutines.*
+import kotlinx.coroutines.GlobalScope.coroutineContext
+import kotlinx.coroutines.flow.*
 import java.io.IOException
+import kotlin.coroutines.CoroutineContext
 
 /**
  * @author yaya (@yahyalmh)
@@ -37,6 +38,24 @@ fun <T> Flow<T>.retryWithPolicy(
     }
 }
 
+fun <T> Flow<T>.retryOnNetworkConnection(
+    connectivityMonitor: ConnectivityMonitor,
+    retryHandler: (e: Throwable) -> Unit
+): Flow<T> = flow {
+    val exception = catchError(this)
+    if (exception != null && exception is IOException) {
+        retryHandler(exception)
+        connectivityMonitor
+            .isOnline
+            .distinctUntilChanged()
+            .collectLatest { isOnline ->
+                if (isOnline) {
+                    collect()
+                }
+            }
+    }
+}
+
 sealed class RetryPolicy(
     val numRetries: Long,
     val delayMillis: Long,
@@ -44,3 +63,47 @@ sealed class RetryPolicy(
 ) {
     object DefaultRetryPolicy : RetryPolicy(numRetries = 6, delayMillis = 1000, delayFactor = 1)
 }
+
+@Suppress("NAME_SHADOWING")
+internal suspend fun <T> Flow<T>.catchError(
+    collector: FlowCollector<T>
+): Throwable? {
+    var fromDownstream: Throwable? = null
+    try {
+        collect {
+            try {
+                collector.emit(it)
+            } catch (e: Throwable) {
+                fromDownstream = e
+                throw e
+            }
+        }
+    } catch (e: Throwable) {
+        val fromDownstream = fromDownstream
+        if (e.isSameExceptionAs(fromDownstream) || e.isCancellationCause(coroutineContext)) {
+            throw e
+        } else {
+            if (fromDownstream == null) {
+                return e
+            }
+            if (e is CancellationException) {
+                fromDownstream.addSuppressed(e)
+                throw fromDownstream
+            } else {
+                e.addSuppressed(fromDownstream)
+                throw e
+            }
+        }
+    }
+    return null
+}
+
+@OptIn(InternalCoroutinesApi::class)
+private fun Throwable.isCancellationCause(coroutineContext: CoroutineContext): Boolean {
+    val job = coroutineContext[Job]
+    if (job == null || !job.isCancelled) return false
+    return isSameExceptionAs(job.getCancellationException())
+}
+
+private fun Throwable.isSameExceptionAs(other: Throwable?): Boolean =
+    other != null && other == this
